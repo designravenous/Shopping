926c5b0362d875828ae596cf09b82dbc023231dd
designravenous <32999911+designravenous@users.noreply.github.com>
2018-12-04 18:56:45 +0100

    Create db_app.py
    Class to connect to DB.

---
+++ db_app.py
@@ -0,0 +1,33 @@
+import pymongo 
+from pymongo import MongoClient
+
+class db_interaction_class:
+    def __init__(self, python_dictionary, db_item, replacement_item):
+        Mongodb_URI = "mongodb://XXX:XXX@ds151292.mlab.com:51292/somedatabase"
+        client = MongoClient(Mongodb_URI, connectTimeoutMS=30000)
+        db = client.get_database('somedatabase')
+        user_records = db.user_baskets
+        self.user_records = user_records
+        self.python_dictionary = python_dictionary
+        self.db_item = db_item
+        self.replacement_item = replacement_item
+        
+    def view_document(self):
+        cursor = self.user_records.find({})
+        for document in cursor:
+            print(document['user_name'],document['item'], document['quantity'], document['date'], document['time'])
+
+    def create_document(self):
+        self.user_records.insert_one(self.python_dictionary)
+    
+    def delete_document(self):
+        try:
+            self.user_records.delete_one({'item':self.db_item})
+        except:
+            print('unable to remove item')
+            
+    def modify_document_item(self):
+        finding_item = self.user_records.find_one({'item':self.db_item})
+        myquery = {'item':finding_item['item']}
+        new_query = {'$set':{'item':self.replacement_item}}
+        self.user_records.update_one(myquery, new_query)
